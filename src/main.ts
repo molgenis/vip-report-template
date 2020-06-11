b4212050b9c32217616e94db510b2f963043456b
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-06-11 13:25:30 +0200

    fix: cleanup

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,10 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from '@/App.vue'
 import i18n from '@/i18n'
-import Api from "@molgenis/vip-report-api"
 import router from '@/router'
 import store from '@/store/store'
 
 Vue.config.productionTip = false
-
-declare global {
-  interface Window {
-    api: any;
-  }
-}
-
-Vue.prototype.$api = new Api(window.api)
 
 new Vue({
   i18n,
