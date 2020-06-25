615198a4e29d132b4a3cb89c0b07c82afe2898a0
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-06-25 15:29:34 +0200

    feature: improve decimal formatting

--- src/globals/filters.ts
+++ src/globals/filters.ts
@@ -1,5 +1,20 @@
-export const numberWithCommas = function (x: number): string {
-    const parts = x.toString().split('.')
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-    return parts.join('.')
+const numberFormatWithGrouping = new Intl.NumberFormat('en-US', {
+    style: 'decimal',
+    maximumFractionDigits: 1
+})
+
+const numberFormatWithoutGrouping = new Intl.NumberFormat('en-US', {
+    style: 'decimal',
+    useGrouping: false,
+    maximumFractionDigits: 1
+})
+
+export const formatNumber = function (x: number, grouping: boolean = false): string {
+    console.log(x)
+    const numberFormat = grouping ? numberFormatWithGrouping : numberFormatWithoutGrouping
+    return numberFormat.format(x)
+}
+
+export const append = function (thisString: string, thatString: string): string {
+    return `${thatString}${thisString}`
 }
