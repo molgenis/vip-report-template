f36f18632602b9cb25fa704ffad8e182038d6e50
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-06-26 11:23:45 +0200

    Sync project configuration with clean vue-cli project

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  collectCoverage: true,
-  "transformIgnorePatterns": [
-    "node_modules/(?!(@molgenis)/)"
-  ]
-}
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  collectCoverage: true
+};
