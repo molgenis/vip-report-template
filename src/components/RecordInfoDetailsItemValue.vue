bf331a506eaf062a02f08e67220d3ea65a8a0c0f
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-06-26 07:08:53 +0200

    feature: outlinks to sequence ontology for SnpEff feature: outlinks to omim for SnpEff feature: outlinks to refseq/ensembl for transcripts

--- src/components/RecordInfoDetailsItemValue.vue
+++ src/components/RecordInfoDetailsItemValue.vue
@@ -1,11 +1,23 @@
 <template>
-    <span v-if="metadataId === 'Consequence'">
+    <span v-if="metadataId === 'Consequence' || metadataId === 'Annotation'">
         <Anchor
                 :href="'http://www.sequenceontology.org/browser/obob.cgi?rm=term_list&obo_query=' + encodeURIComponent(value) + '&release=current_release'"
                 :text="value"/>
     </span>
-    <span v-else-if="metadataId === 'SYMBOL'">
-        <Anchor :href="'https://www.omim.org/search?search=' + encodeURIComponent(value) + '&field=approved_gene_symbol'" :text="value" />
+    <span v-else-if="metadataId === 'SYMBOL' || metadataId === 'Gene_Name'">
+        <Anchor :href="'https://www.omim.org/search?search=' + encodeURIComponent(value) + '&field=approved_gene_symbol'"
+                :text="value"/>
+    </span>
+    <span v-else-if="metadataId === 'Feature' || metadataId === 'Feature_ID'">
+        <Anchor v-if="isRefSeqFeatureId()"
+                :href="'https://www.ncbi.nlm.nih.gov/nuccore/' + encodeURIComponent(value)"
+                :text="value"/>
+        <Anchor v-else-if="isEnsemblFeatureId()"
+                :href="'http://www.ensembl.org/Homo_sapiens/Transcript/Summary?db=core;t=' + encodeURIComponent(value)"
+                :text="value"/>
+        <span v-else>
+            {{ value }}
+        </span>
     </span>
     <span v-else>
         {{ value }}
@@ -22,6 +34,33 @@
         props: {
             metadataId: String as PropType<String>,
             value: [String, Number, Boolean] as PropType<string | number | boolean>
+        },
+        methods: {
+            isRefSeqFeatureId(): boolean {
+                let isRefSeqFeatureId
+                if (typeof this.value === 'string') {
+                    isRefSeqFeatureId =
+                        this.value.startsWith('NM_') ||
+                        this.value.startsWith('NR_') ||
+                        this.value.startsWith('NP_') ||
+                        this.value.startsWith('XM_') ||
+                        this.value.startsWith('XR_') ||
+                        this.value.startsWith('XP_')
+                } else {
+                    isRefSeqFeatureId = false
+                }
+                console.log(this.value, isRefSeqFeatureId)
+                return isRefSeqFeatureId
+            },
+            isEnsemblFeatureId(): boolean {
+                let isEnsemblFeatureId
+                if (typeof this.value === 'string') {
+                    isEnsemblFeatureId = this.value.startsWith('ENST')
+                } else {
+                    isEnsemblFeatureId = false
+                }
+                return isEnsemblFeatureId
+            }
         }
     })
 </script>
