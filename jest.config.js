5c9a6233c373a00622c289c26cd328ebdddeccc1
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-06-29 13:04:58 +0200

    fix formatting

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   collectCoverage: true,
-  transformIgnorePatterns: [
-    "node_modules/(?!(@molgenis)/)"
-  ]
+  transformIgnorePatterns: ['node_modules/(?!(@molgenis)/)']
 };
