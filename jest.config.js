f62795121fc378cd67e6ec9c824f8f92b3b969ff
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-06-29 09:31:47 +0200

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  collectCoverage: true
+  collectCoverage: true,
+  transformIgnorePatterns: [
+    "node_modules/(?!(@molgenis)/)"
+  ]
 };
