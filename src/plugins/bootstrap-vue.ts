efe4f669c0ebbaed88e3b796711dda3baed90071
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-06-30 11:32:27 +0200

    add chevron icons

--- src/plugins/bootstrap-vue.ts
+++ src/plugins/bootstrap-vue.ts
@@ -1,10 +1,20 @@
 import Vue from 'vue';
-import { BootstrapVue, BIcon, BIconBoxArrowInUpRight, BIconQuestionCircle, BIconSearch } from 'bootstrap-vue';
+import {
+  BootstrapVue,
+  BIcon,
+  BIconBoxArrowInUpRight,
+  BIconChevronDown,
+  BIconChevronUp,
+  BIconQuestionCircle,
+  BIconSearch
+} from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
 Vue.component('BIcon', BIcon);
 Vue.component('BIconBoxArrowInUpRight', BIconBoxArrowInUpRight);
+Vue.component('BIconChevronDown', BIconChevronDown);
+Vue.component('BIconChevronUp', BIconChevronUp);
 Vue.component('BIconQuestionCircle', BIconQuestionCircle);
 Vue.component('BIconSearch', BIconSearch);
