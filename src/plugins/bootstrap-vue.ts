db68ef6cfbfccd5ab77d6c73ddf5e2473def7378
Dennis Hendriksen <d.hendriksen@umcg.nl>
2020-09-22 13:22:12 +0200

    feat: import only specific components groups for bootstrap-vue

--- src/plugins/bootstrap-vue.ts
+++ src/plugins/bootstrap-vue.ts
@@ -1,6 +1,17 @@
 import Vue from 'vue';
 import {
-  BootstrapVue,
+  AlertPlugin,
+  ButtonPlugin,
+  CollapsePlugin,
+  FormGroupPlugin,
+  FormInputPlugin,
+  LayoutPlugin,
+  ModalPlugin,
+  NavbarPlugin,
+  NavPlugin,
+  PaginationPlugin,
+  TablePlugin,
+  VBTooltipPlugin,
   BIcon,
   BIconBoxArrowInUpRight,
   BIconChevronDown,
@@ -11,7 +22,18 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.use(BootstrapVue);
+Vue.use(AlertPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(CollapsePlugin);
+Vue.use(FormGroupPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(LayoutPlugin);
+Vue.use(ModalPlugin);
+Vue.use(NavbarPlugin);
+Vue.use(NavPlugin);
+Vue.use(PaginationPlugin);
+Vue.use(TablePlugin);
+Vue.use(VBTooltipPlugin);
 Vue.component('BIcon', BIcon);
 Vue.component('BIconBoxArrowInUpRight', BIconBoxArrowInUpRight);
 Vue.component('BIconChevronDown', BIconChevronDown);
