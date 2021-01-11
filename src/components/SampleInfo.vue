6ae26e190b4548752869515b002c327e4c66b3cb
bartcharbon <bart.charbon@gmail.com>
2021-01-11 14:50:42 +0100

    Add pedigree information (sex, affected status)

--- src/components/SampleInfo.vue
+++ src/components/SampleInfo.vue
@@ -1,20 +1,33 @@
 <template>
   <b-row>
-    <b-col cols="1">
-      <span>{{ $t('phenotypes') }}:</span>
-    </b-col>
     <b-col>
-      <SamplePheno :phenotypes="phenotypes" />
+      <b-row>
+        <b-col cols="1">
+          <span>{{ $t('pedigree') }}:</span>
+        </b-col>
+        <b-col>
+          <SamplePedigree :sample="sample" />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="1">
+          <span>{{ $t('phenotypes') }}:</span>
+        </b-col>
+        <b-col>
+          <SamplePheno :phenotypes="phenotypes" />
+        </b-col>
+      </b-row>
     </b-col>
   </b-row>
 </template>
 
 <script lang="ts">
 import SamplePheno from '@/components/SamplePheno.vue';
+import SamplePedigree from '@/components/SamplePedigree.vue';
 import Vue from 'vue';
 
 export default Vue.extend({
-  components: { SamplePheno },
+  components: { SamplePheno, SamplePedigree },
   props: {
     sample: Object,
     phenotypes: Object
