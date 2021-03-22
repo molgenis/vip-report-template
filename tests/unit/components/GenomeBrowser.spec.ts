b92b1d605cb4441c36be839cecc2c52c985ff028
Dennis Hendriksen <d.hendriksen@umcg.nl>
2021-03-22 11:45:34 +0100

    add unit tests

--- tests/unit/components/GenomeBrowser.spec.ts
+++ tests/unit/components/GenomeBrowser.spec.ts
@@ -1,5 +1,5 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
-import Vuex from 'vuex';
+import Vuex, { GetterTree, Store } from 'vuex';
 import GenomeBrowser from '@/components/GenomeBrowser.vue';
 
 const localVue = createLocalVue();
@@ -7,14 +7,49 @@ const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe('GenomeBrowser', () => {
-  let wrapper: any;
+  const genomeBrowserDbFn = jest.fn();
+
+  let store: Store<never>;
 
   beforeEach(() => {
-    const store = new Vuex.Store({});
-    wrapper = shallowMount(GenomeBrowser, { store, localVue });
+    const getters: GetterTree<never, never> = { genomeBrowserDb: genomeBrowserDbFn };
+
+    store = new Vuex.Store({ getters });
   });
 
-  it('should render the component', () => {
+  it('should not render a div for genomeBrowserDb hg16', () => {
+    genomeBrowserDbFn.mockReturnValue('hg16');
+    const wrapper = shallowMount(GenomeBrowser, { store, localVue });
     expect(wrapper).toBeDefined();
+  });
+
+  it('should not render a div for genomeBrowserDb hg17', () => {
+    genomeBrowserDbFn.mockReturnValue('hg17');
+    const wrapper = shallowMount(GenomeBrowser, { store, localVue });
+    expect(wrapper.find('div').exists()).toBe(false);
+  });
+
+  it('should not render a div for genomeBrowserDb hg18', () => {
+    genomeBrowserDbFn.mockReturnValue('hg18');
+    const wrapper = shallowMount(GenomeBrowser, { store, localVue });
+    expect(wrapper.find('div').exists()).toBe(false);
+  });
+
+  it('should render a div for genomeBrowserDb hg19', () => {
+    genomeBrowserDbFn.mockReturnValue('hg19');
+    const wrapper = shallowMount(GenomeBrowser, { store, localVue });
+    expect(wrapper.find('div').exists()).toBe(true);
+  });
+
+  it('should render a div for genomeBrowserDb hg38', () => {
+    genomeBrowserDbFn.mockReturnValue('hg38');
+    const wrapper = shallowMount(GenomeBrowser, { store, localVue });
+    expect(wrapper.find('div').exists()).toBe(true);
+  });
+
+  it('should not render a div for unknown genomeBrowserDb', () => {
+    genomeBrowserDbFn.mockReturnValue(null);
+    const wrapper = shallowMount(GenomeBrowser, { store, localVue });
+    expect(wrapper.find('div').exists()).toBe(false);
   });
 });
