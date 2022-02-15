09981b912e8258ea3e7c09c18d2c0263df5c59b9
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-02-15 16:37:55 +0100

    fix linting error

--- src/utils/inject.ts
+++ src/utils/inject.ts
@@ -3,7 +3,7 @@ import { inject, InjectionKey } from "vue";
 function injectStrict<T>(key: InjectionKey<T>, fallback?: T) {
   const resolved = inject(key, fallback);
   if (!resolved) {
-    throw new Error(`Could not resolve ${key.description}`);
+    throw new Error(`Could not resolve ${key.description || "undefined"}`);
   }
 
   return resolved;
