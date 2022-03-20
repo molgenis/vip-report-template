8e038e53bf812eb2c6c5774a5853137fbb9060d9
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-03-20 17:22:08 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["prettier", "@typescript-eslint"],
+};
