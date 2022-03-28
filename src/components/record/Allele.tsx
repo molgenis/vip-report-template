55e6ef2d9c64a67116f79dfdf7f48209696dd958
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-03-28 14:34:47 +0200

    upadte Allele component: abbreviation, breakend, symbolic

--- src/components/record/Allele.tsx
+++ src/components/record/Allele.tsx
@@ -1,27 +1,44 @@
 import { Component, For } from "solid-js";
 
 export const Allele: Component<{ value: string | null }> = (props) => {
-  // TODO abbreviate long alleles, config via props
-  // TODO support breakends, symbolic alleles
-  const bases = props.value ? [...props.value] : ["?"];
+  const missing = props.value === null;
+  const symbolic = props.value?.startsWith("<") && props.value?.endsWith(">");
+  const breakend =
+    props.value?.indexOf("[") !== -1 || props.value?.indexOf("]") !== -1 || props.value?.indexOf(".") !== -1;
+
+  function nucs(value: string) {
+    let nucleotides = value.split("");
+    if (nucleotides.length > 4) {
+      const lastNuc = nucleotides[nucleotides.length - 1];
+      nucleotides = nucleotides.slice(0, 2);
+      nucleotides.push("\u2026"); // ellipsis
+      nucleotides.push(lastNuc);
+    }
+    return nucleotides;
+  }
+
   return (
     <>
-      <For each={bases}>
-        {(base) => (
-          <span
-            classList={{
-              base: true,
-              "base-a": base === "A",
-              "base-c": base === "C",
-              "base-g": base === "G",
-              "base-n": base === "N",
-              "base-t": base === "T",
-            }}
-          >
-            {base}
-          </span>
-        )}
-      </For>
+      {missing && <span classList={{ base: true, "base-m": true }}>?</span>}
+      {(symbolic || breakend) && <span class="base-n">{props.value}</span>}
+      {!missing && !symbolic && !breakend && (
+        <For each={nucs(props.value as string)}>
+          {(base) => (
+            <span
+              classList={{
+                base: true,
+                "base-a": base === "A",
+                "base-c": base === "C",
+                "base-g": base === "G",
+                "base-n": base === "N",
+                "base-t": base === "T",
+              }}
+            >
+              {base}
+            </span>
+          )}
+        </For>
+      )}
     </>
   );
 };
