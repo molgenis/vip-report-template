3d40889a4676160127db9424899d9fd3032b8768
mkslofstra <mariska-karina@hotmail.com>
2022-05-05 11:07:52 +0200

    improvement(breadcrumb): make component for breadcrumb

---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,53 @@
+import { Link } from "solid-app-router";
+import { Component, For, Show } from "solid-js";
+
+export const Breadcrumb: Component<{
+  links: { href: string; label: string }[];
+}> = (props) => {
+  return (
+    <div class="columns is-gapless">
+      <div class="column">
+        <nav class="breadcrumb">
+          <ul>
+            <Show
+              when={props.links.length == 0}
+              fallback={
+                <li>
+                  <Link href="/">
+                    <span class="icon">
+                      <i class="fa-solid fa-home" />
+                    </span>
+                  </Link>
+                </li>
+              }
+            >
+              <li class="is-active">
+                <Link href="#">
+                  <span class="icon">
+                    <i class="fa-solid fa-home" />
+                  </span>
+                </Link>
+              </li>
+            </Show>
+            <For each={props.links}>
+              {(value) => (
+                <Show
+                  when={value.href == "#"}
+                  fallback={
+                    <li>
+                      <Link href={value.href}>{value.label}</Link>
+                    </li>
+                  }
+                >
+                  <li class="is-active">
+                    <Link href={value.href}>{value.label}</Link>
+                  </li>
+                </Show>
+              )}
+            </For>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
