05e57ab82ec79e633cc756f693bc9f537fa9c0ee
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-05-19 08:17:01 +0200

    fix:add tests

---
+++ test/utils/field.test.tsx
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "vitest";
+import { isNumerical } from "../../src/utils/field";
+import { FieldMetadata } from "@molgenis/vip-report-vcf/src/MetadataParser";
+
+describe("field utilities", () => {
+  test("FLOAT is numerical", () => {
+    expect(isNumerical({ type: "FLOAT" } as FieldMetadata)).toBe(true);
+  });
+
+  test("INTEGER is numerical", () => {
+    expect(isNumerical({ type: "INTEGER" } as FieldMetadata)).toBe(true);
+  });
+
+  test("STRING is not numerical", () => {
+    expect(isNumerical({ type: "STRING" } as FieldMetadata)).toBe(false);
+  });
+});
