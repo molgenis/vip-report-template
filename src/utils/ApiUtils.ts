cf1ad67c29c5ad7ccba8ebfd8475885d466018a3
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-06-02 11:03:32 +0200

    fix:sort consequences based on sort params or sort default

--- src/utils/ApiUtils.ts
+++ src/utils/ApiUtils.ts
@@ -9,9 +9,13 @@ import {
   Phenotype,
   Resource,
   Sample,
+  SortOrder,
 } from "@molgenis/vip-report-api/src/Api";
 import { Metadata, Record } from "@molgenis/vip-report-vcf/src/Vcf";
 import api from "../Api";
+import { Value } from "@molgenis/vip-report-vcf/src/ValueParser";
+import { FieldMetadata, NestedFieldMetadata } from "@molgenis/vip-report-vcf/src/MetadataParser";
+import { isNumerical } from "./field";
 
 export const EMPTY_PARAMS: Params = {};
 
@@ -29,8 +33,124 @@ export const EMPTY_SAMPLES_PAGE = createEmptyPagedItems() as PagedItems<Sample>;
 
 export const EMPTY_SAMPLES = [] as Sample[];
 
+const consequenceOrder = [
+  "transcript_ablation",
+  "splice_acceptor_variant",
+  "splice_donor_variant",
+  "splice_donor_5th_base_variant",
+  "splice_donor_region_variant",
+  "splice_polypyrimidine_tract_variant",
+  "stop_gained",
+  "frameshift_variant",
+  "stop_lost",
+  "start_lost",
+  "transcript_amplification",
+  "inframe_insertion",
+  "inframe_deletion",
+  "missense_variant",
+  "protein_altering_variant",
+  "splice_region_variant",
+  "incomplete_terminal_codon_variant",
+  "start_retained_variant",
+  "stop_retained_variant",
+  "synonymous_variant",
+  "coding_sequence_variant",
+  "mature_miRNA_variant",
+  "5_prime_UTR_variant",
+  "3_prime_UTR_variant",
+  "non_coding_transcript_exon_variant",
+  "intron_variant",
+  "NMD_transcript_variant",
+  "non_coding_transcript_variant",
+  "upstream_gene_variant",
+  "downstream_gene_variant",
+  "TFBS_ablation",
+  "TFBS_amplification",
+  "TF_binding_site_variant",
+  "regulatory_region_ablation",
+  "regulatory_region_amplification",
+  "feature_elongation",
+  "regulatory_region_variant",
+  "feature_truncation",
+  "intergenic_variant",
+].reduce((acc: { [key: string]: number }, curr, currIndex) => ((acc[curr] = currIndex), acc), {});
+
+function getMostSevereConsequenceIndex(value: string[]) {
+  return value.map((consequence) => consequenceOrder[consequence]).reduce((max, value) => Math.max(max, value));
+}
+
+function getCsqSortOrders(params: Params): SortOrder[] {
+  const sortOrders = params.sort ? (Array.isArray(params.sort) ? params.sort : [params.sort]) : [];
+  return sortOrders.filter(
+    (sortOrder) =>
+      (sortOrder.compare === undefined || typeof sortOrder.compare !== "function") &&
+      typeof sortOrder.property !== "string" &&
+      sortOrder.property.parent?.id === "CSQ" &&
+      isNumerical(sortOrder.property) &&
+      sortOrder.property.number.type === "NUMBER" &&
+      sortOrder.property.number.count === 1
+  );
+}
+
+function compareCsqValue(aValue: number | null, bValue: number | null): number {
+  if (aValue === null) return bValue !== null ? -1 : 0;
+  if (bValue === null) return 1;
+  return aValue - bValue;
+}
+
+function compareCsq(aValueArray: Value[], bValueArray: Value[], field: FieldMetadata): number {
+  const parentField = field.parent as FieldMetadata;
+  const parentItems = (parentField.nested as NestedFieldMetadata).items;
+
+  const index = parentItems.findIndex((item) => item.id === field.id);
+  if (index === -1) {
+    throw new Error(`unknown field '${field.id}'`);
+  }
+
+  const aValue = aValueArray[index] as number | null;
+  const bValue = bValueArray[index] as number | null;
+  return compareCsqValue(aValue, bValue);
+}
+
+function compareCsqDefault(aValue: Value[], bValue: Value[], pickIndex: number, consequenceIndex: number): number {
+  if (pickIndex !== -1) {
+    if (aValue[pickIndex] === "1") return bValue[pickIndex] === null ? -1 : 0;
+    if (bValue[pickIndex] === "1") return 1;
+  }
+
+  if (consequenceIndex !== -1) {
+    const aIndex = getMostSevereConsequenceIndex(aValue[consequenceIndex] as string[]);
+    const bIndex = getMostSevereConsequenceIndex(bValue[consequenceIndex] as string[]);
+    return aIndex - bIndex;
+  } else {
+    return 0;
+  }
+}
+
 export async function fetchRecords(params: Params) {
-  return await api.getRecords(params);
+  const [recordsMeta, records] = await Promise.all([api.getRecordsMeta(), api.getRecords(params)]);
+  if (recordsMeta.info.CSQ === undefined) {
+    return records;
+  }
+
+  const sortOrders = getCsqSortOrders(params);
+
+  const fieldMetas = (recordsMeta.info.CSQ.nested as NestedFieldMetadata).items;
+  const consequenceIndex = fieldMetas.findIndex((item) => item.id === "Consequence");
+  const pickIndex = fieldMetas.findIndex((item) => item.id === "PICK");
+
+  for (const record of records.items) {
+    const csqArray = record.data.n.CSQ as Value[][];
+
+    csqArray.sort((aValue, bValue) => {
+      for (const sortOrder of sortOrders) {
+        const compareValue = compareCsq(aValue, bValue, sortOrder.property as FieldMetadata);
+        if (compareValue !== 0) return sortOrder.compare !== "desc" ? compareValue : -compareValue;
+      }
+      return compareCsqDefault(aValue, bValue, pickIndex, consequenceIndex);
+    });
+  }
+  return records;
 }
 
 export async function fetchRecordsMeta() {
