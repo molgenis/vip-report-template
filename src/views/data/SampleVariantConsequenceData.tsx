55056e26fd3b720167a37f0c24a306bb48c60e8b
mkslofstra <mariska-karina@hotmail.com>
2022-06-03 09:48:41 +0200

    fix(consequencedata): convert string to int to fit typing

--- src/views/data/SampleVariantConsequenceData.tsx
+++ src/views/data/SampleVariantConsequenceData.tsx
@@ -5,6 +5,6 @@ import { fetchRecordById, fetchSampleById } from "../../utils/ApiUtils";
 export default function SampleVariantConsequenceData({ params }: RouteDataFuncArgs) {
   const [sample] = createResource(() => params.sampleId, fetchSampleById);
   const [variant] = createResource(() => params.variantId, fetchRecordById);
-  const consequenceId: string = params.consequenceId;
+  const consequenceId: number = parseInt(params.consequenceId);
   return { sample, variant, consequenceId };
 }
