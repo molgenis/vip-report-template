71c2b8f596c716a3755d89dfab853c29f10deef1
bartcharbon <bart.charbon@gmail.com>
2022-06-13 16:10:40 +0200

    Fix warnings in VariantsTable.

--- src/components/VariantsTable.tsx
+++ src/components/VariantsTable.tsx
@@ -15,7 +15,7 @@ import { Item } from "@molgenis/vip-report-api/src/Api";
 import { FieldHeader } from "./FieldHeader";
 
 const computeRowspan = (recordsMetadata: Metadata) =>
-  Object.values(recordsMetadata.info).find((field) => field.nested) !== undefined ? 2 : undefined;
+  Object.values(recordsMetadata.info).find((field) => field.nested) !== undefined ? 2 : 1;
 const computeColspan = (field: FieldMetadata) => (field.nested ? field.nested.items.length : undefined);
 
 export const VariantsTable: Component<{
