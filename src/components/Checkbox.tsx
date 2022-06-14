efccfdf380840b41ceec7ab4d5a275c1d7b5dd64
Dennis Hendriksen <d.hendriksen@umcg.nl>
2022-06-14 13:46:13 +0200

    fix(typescript):solid/reactivity

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,4 +1,4 @@
-import { Component, createSignal, Show } from "solid-js";
+import { Component, Show } from "solid-js";
 import { Abbr } from "./Abbr";
 
 export type CheckboxEvent = {
@@ -13,11 +13,8 @@ export const Checkbox: Component<{
   checked?: boolean;
   onChange: (event: CheckboxEvent) => void;
 }> = (props) => {
-  const [checked, setChecked] = createSignal(!!props.checked);
-
   const onChange = (event: Event) => {
     const target = event.target as HTMLInputElement;
-    setChecked(target.checked);
     props.onChange({
       value: target.value,
       checked: target.checked,
@@ -26,7 +23,7 @@ export const Checkbox: Component<{
 
   return (
     <label class="checkbox">
-      <input class="mr-1" type="checkbox" value={props.value} checked={checked()} onChange={onChange} />
+      <input class="mr-1" type="checkbox" value={props.value} checked={props.checked} onChange={onChange} />
       <Show when={props.desc !== undefined} fallback={<span>{props.label}</span>}>
         <Abbr title={props.desc as string} value={props.label} />
       </Show>
