ac1dbee8be491383587d733e108f96bf47456dbd
bartcharbon <bart.charbon@gmail.com>
2022-06-17 08:06:42 +0200

    Fix: VIBE linkout does not open in a new tab

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -1,8 +1,8 @@
-import { Component } from "solid-js";
+import { Component, JSXElement } from "solid-js";
 
 export const Anchor: Component<{
   href: string;
-  value: string | number;
+  value: string | number | JSXElement;
 }> = (props) => {
   return (
     <a href={props.href} target="_blank" rel="noopener noreferrer nofollow">
