94d44b6211999ece120e1befc6fb20723dfa7850
Dennis Hendriksen <d.hendriksen@umcg.nl>
2023-02-17 08:27:46 +0100

    Fix #252 Unicode character rendering broken

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import inlinePlugin from "@molgenis/vite-plugin-inline/src/vite-plugin-inline.js
 
 export default defineConfig({
   plugins: [solidPlugin(), inlinePlugin()],
+  esbuild: {
+    // @molgenis/vite-plugin-inline requires ascii input and cannot handle UTF-8 input
+    charset: "ascii",
+  },
   build: {
     polyfillDynamicImport: false,
     // inline plugin build options
