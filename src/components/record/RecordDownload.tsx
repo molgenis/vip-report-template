cdfd6216aad7ac932a22814172203483092770e2
dennishendriksen <d.hendriksen@umcg.nl>
2023-11-02 10:18:28 +0100

    Timestamp download filenames

--- src/components/record/RecordDownload.tsx
+++ src/components/record/RecordDownload.tsx
@@ -4,13 +4,32 @@ import { Metadata } from "@molgenis/vip-report-vcf/src/Vcf";
 import api from "../../Api";
 import { Filter, writeVcf } from "@molgenis/vip-report-vcf/src/VcfWriter";
 
-const DOWNLOAD_POSTFIX = "_report.vcf";
 export const RecordDownload: Component<{ recordsMetadata: Metadata; query?: Query; samples?: Sample[] }> = (props) => {
   const filter = (): Filter | undefined =>
     props.samples ? { samples: props.samples.map((sample) => sample.person.individualId) } : undefined;
 
   function parseFilename(htsFile: HtsFileMetadata) {
     return htsFile.uri.split("\\").pop()?.split("/").pop()?.split(".").shift();
+  }
+
+  // derived from https://stackoverflow.com/a/19176102
+  function getDateTimeString() {
+    const now = new Date();
+
+    const year = now.getFullYear().toString();
+    let month = (now.getMonth() + 1).toString();
+    let day = now.getDate().toString();
+    let hour = now.getHours().toString();
+    let minute = now.getMinutes().toString();
+    let second = now.getSeconds().toString();
+
+    if (month.length == 1) month = "0" + month;
+    if (day.length == 1) day = "0" + day;
+    if (hour.length == 1) hour = "0" + hour;
+    if (minute.length == 1) minute = "0" + minute;
+    if (second.length == 1) second = "0" + second;
+
+    return year + month + day + "_" + hour + minute + second;
   }
 
   function onClick() {
@@ -23,7 +42,7 @@ export const RecordDownload: Component<{ recordsMetadata: Metadata; query?: Quer
       link.href = url;
       const htsFile = await api.getHtsFileMetadata();
       const filename = parseFilename(htsFile) as string;
-      link.setAttribute("download", filename + DOWNLOAD_POSTFIX);
+      link.setAttribute("download", filename + "_report_" + getDateTimeString() + ".vcf");
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
