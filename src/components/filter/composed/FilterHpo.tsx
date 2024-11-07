db5ff4babca3b27c1caf7d44f6a35852d844063f
bart.charbon <b.charbon@umcg.nl>
2024-11-07 15:01:45 +0100

    Fix ESLint error

--- src/components/filter/composed/FilterHpo.tsx
+++ src/components/filter/composed/FilterHpo.tsx
@@ -1,8 +1,16 @@
 import { Component } from "solid-js";
 import { FilterTyped } from "../typed/FilterTyped.tsx";
-import { FilterProps } from "../Filter.tsx";
+import { FilterProps, FilterValueChangeCallback } from "../Filter.tsx";
 import { ConfigFilterHpo, FilterValueHpo } from "../../../types/configFilterComposed";
+import { ConfigFilterField, FilterValueCategorical, FilterValueField } from "../../../types/configFilter";
 
 export const FilterHpo: Component<FilterProps<ConfigFilterHpo, FilterValueHpo>> = (props) => {
-  return <FilterTyped {...props} />;
+  return (
+    <FilterTyped
+      config={props.config as ConfigFilterField}
+      value={props.value as FilterValueCategorical}
+      onValueChange={props.onValueChange as FilterValueChangeCallback<FilterValueField>}
+      onValueClear={props.onValueClear}
+    />
+  );
 };
