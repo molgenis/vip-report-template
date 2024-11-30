322bb9ad7b95dad529105e4f101f8409dd8ac738
dennishendriksen <d.hendriksen@umcg.nl>
2024-11-30 17:15:48 +0100

    fix:variant consequence table categorical rendering

--- src/components/VariantConsequenceTable.tsx
+++ src/components/VariantConsequenceTable.tsx
@@ -1,58 +1,49 @@
-import { Component, createMemo, For, Show } from "solid-js";
-import { FieldMetadata, ValueArray, VcfRecord } from "@molgenis/vip-report-vcf";
+import { Component, For } from "solid-js";
+import { VcfRecord } from "@molgenis/vip-report-vcf";
 import { Item } from "@molgenis/vip-report-api";
-import { FieldTyped } from "./field/typed/FieldTyped.tsx";
 import { Table } from "./Table.tsx";
-import { abbreviateHeader } from "../utils/utils.ts";
+import { initConfigCells } from "../utils/config/configCells.ts";
+import { RecordsTableCellLine, RecordsTableHeaderCell } from "./RecordsTable.tsx";
+import { VariantType } from "../utils/variantType.ts";
+import { MetadataContainer } from "../utils/api.ts";
 
 export const VariantConsequenceTable: Component<{
-  csqMetadata: FieldMetadata[];
-  csqValues: ValueArray;
+  variantType: VariantType;
+  metadata: MetadataContainer;
   record: Item<VcfRecord>;
+  consequenceId: number;
 }> = (props) => {
-  function getValues(index: number): ValueArray {
-    return props.csqValues[index] !== null ? (props.csqValues[index] as ValueArray) : [];
-  }
+  const configCells = () =>
+    initConfigCells(
+      [
+        {
+          type: "info",
+          name: "CSQ/.*",
+        },
+      ],
+      props.variantType,
+      props.metadata,
+      null,
+    ).filter((configCell) => configCell.type !== "group");
 
   return (
     <Table borderless={true}>
       <tbody>
-        <For each={props.csqMetadata}>
-          {(field: FieldMetadata, index) => (
-            <Show when={!(getValues(index()).length === 0)}>
-              <tr>
-                <FieldHeader field={field} />
-                <td>
-                  <FieldTyped metadata={field} value={props.csqValues[index()]} />
-                </td>
-              </tr>
-            </Show>
+        <For each={configCells()}>
+          {(fieldConfig) => (
+            <tr>
+              <RecordsTableHeaderCell fieldConfig={fieldConfig} />
+              <td>
+                <RecordsTableCellLine
+                  fieldConfig={fieldConfig}
+                  record={props.record}
+                  valueIndex={props.consequenceId}
+                />
+              </td>
+            </tr>
           )}
         </For>
       </tbody>
     </Table>
-  );
-};
-
-const FieldHeader: Component<{
-  field: FieldMetadata;
-  vertical?: boolean;
-  rowspan?: number;
-  colspan?: number;
-}> = (props) => {
-  const label = createMemo(() => abbreviateHeader(props.field.label || props.field.id));
-  return (
-    <th
-      style={props.vertical ? { "writing-mode": "vertical-rl" } : undefined}
-      rowspan={props.rowspan}
-      colspan={props.colspan}
-    >
-      <Show
-        when={props.field.description && props.field.description !== label()}
-        fallback={<abbr title={props.field.label || props.field.id}>{label()}</abbr>}
-      >
-        <abbr title={props.field.description}>{label()}</abbr>
-      </Show>
-    </th>
   );
 };
