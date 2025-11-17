91fdda257387713843570760c414a7bad1d033fd
bart.charbon <b.charbon@umcg.nl>
2025-11-17 10:08:30 +0100

    Disable locus filter if contig is not set

--- src/components/form/ButtonApply.tsx
+++ src/components/form/ButtonApply.tsx
@@ -1,8 +1,12 @@
 import { Component } from "solid-js";
 
-export const ButtonApply: Component<{ onClick: () => void }> = (props) => {
+export const ButtonApply: Component<{ onClick: () => void; isEnabled?: () => boolean }> = (props) => {
   return (
-    <button class="button is-small is-info" onClick={() => props.onClick()}>
+    <button
+      class="button is-small is-info"
+      onClick={() => props.onClick()}
+      disabled={props.isEnabled !== undefined && !props.isEnabled()}
+    >
       <span class="icon">
         <i class="fas fa-angle-right" />
       </span>
