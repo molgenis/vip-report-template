fddca0702cfb7ed68e0ed222a3ed56eca2b4daa9
bart.charbon <b.charbon@umcg.nl>
2026-01-21 12:06:21 +0100

    Revert change

--- src/components/filter/composed/FilterHpo.tsx
+++ src/components/filter/composed/FilterHpo.tsx
@@ -2,13 +2,14 @@ import { Component } from "solid-js";
 import { FilterTyped } from "../typed/FilterTyped.tsx";
 import { FilterProps, FilterValueChangeCallback } from "../Filter.tsx";
 import { ConfigFilterHpo, FilterValueHpo } from "../../../types/configFilterComposed";
-import { FilterValueCategorical, FilterValueField } from "../../../types/configFilter";
+import { ConfigFilterField, FilterValueCategorical, FilterValueField } from "../../../types/configFilter";
 
 export const FilterHpo: Component<FilterProps<ConfigFilterHpo, FilterValueHpo>> = (props) => {
   return (
     <FilterTyped
-      config={props.config}
+      config={props.config as ConfigFilterField}
       value={props.value as FilterValueCategorical}
+      defaultValue={props.defaultValue}
       onValueChange={props.onValueChange as FilterValueChangeCallback<FilterValueField>}
       onValueClear={props.onValueClear}
       isInited={props.isInited}
