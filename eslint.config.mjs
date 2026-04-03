19133eb6f9b2b6fcc6422b871c4db678fa46f81a
bart.charbon <b.charbon@umcg.nl>
2026-04-03 12:03:10 +0200

    merge with main

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,12 @@
-import pluginJs from "@eslint/js";
 // workaround: import .js because of https://github.com/solidjs-community/eslint-plugin-solid/issues/118
 import solid from "eslint-plugin-solid/configs/typescript";
 import tseslint from "typescript-eslint";
 
 export default [
   {
-    ignores: [
-      "**/*.precompiled.ts"
-    ]
+    ignores: ["**/*.precompiled.ts"],
   },
-  { files: ["**/*.{js,mjs,cjs,ts,tsx}"] },
-  pluginJs.configs.recommended,
+  { files: ["**/*.{ts,tsx}"] },
   solid,
   ...tseslint.configs.recommended,
 ];
