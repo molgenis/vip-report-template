4f11e9a01fcab1f552011081a4bcd449fa190eb8
Bart Charbon <b.charbon@umcg.nl>
2026-04-16 07:47:16 +0200

    Feat: add switch to toggle empty fields (#377)

--- src/components/VariantConsequenceTable.tsx
+++ src/components/VariantConsequenceTable.tsx
@@ -1,4 +1,4 @@
-import { Component, For } from "solid-js";
+import { Component, For, Show } from "solid-js";
 import { VcfRecord } from "@molgenis/vip-report-vcf";
 import { Item } from "@molgenis/vip-report-api";
 import { Table } from "./Table.tsx";
@@ -6,12 +6,14 @@ import { initConfigCells } from "../utils/config/configCells.ts";
 import { RecordsTableCellLine, RecordsTableHeaderCell } from "./RecordsTable.tsx";
 import { VariantType } from "../utils/variantType.ts";
 import { MetadataContainer } from "../utils/api.ts";
+import { ConfigCellInfo } from "../types/configCells";
 
 export const VariantConsequenceTable: Component<{
   variantType: VariantType;
   metadata: MetadataContainer;
   record: Item<VcfRecord>;
   consequenceId: number;
+  isShowEmpty: boolean;
 }> = (props) => {
   const configCells = () =>
     initConfigCells(
@@ -26,21 +28,28 @@ export const VariantConsequenceTable: Component<{
       null,
     ).filter((configCell) => configCell.type !== "group");
 
+  function isEmptyValue(fieldConfig: ConfigCellInfo) {
+    const value = fieldConfig.value(props.record, props.consequenceId);
+    return value === null || value === undefined || (Array.isArray(value) && value.length === 0);
+  }
+
   return (
     <Table borderless={true}>
       <tbody>
-        <For each={configCells()}>
-          {(fieldConfig) => (
-            <tr>
-              <RecordsTableHeaderCell fieldConfig={fieldConfig} />
-              <td>
-                <RecordsTableCellLine
-                  fieldConfig={fieldConfig}
-                  record={props.record}
-                  valueIndex={props.consequenceId}
-                />
-              </td>
-            </tr>
+        <For each={configCells() as ConfigCellInfo[]}>
+          {(fieldConfig: ConfigCellInfo) => (
+            <Show when={!isEmptyValue(fieldConfig) || props.isShowEmpty}>
+              <tr>
+                <RecordsTableHeaderCell fieldConfig={fieldConfig} showParentHeader={true} />
+                <td>
+                  <RecordsTableCellLine
+                    fieldConfig={fieldConfig}
+                    record={props.record}
+                    valueIndex={props.consequenceId}
+                  />
+                </td>
+              </tr>
+            </Show>
           )}
         </For>
       </tbody>
--- src/components/VariantInfoTable.tsx
+++ src/components/VariantInfoTable.tsx
@@ -1,4 +1,4 @@
-import { Component, For } from "solid-js";
+import { Component, For, Show } from "solid-js";
 import { VcfRecord } from "@molgenis/vip-report-vcf";
 import { Table } from "./Table.tsx";
 import { Item } from "@molgenis/vip-report-api";
@@ -6,11 +6,13 @@ import { RecordsTableCell, RecordsTableHeaderCell } from "./RecordsTable.tsx";
 import { initConfigCells } from "../utils/config/configCells.ts";
 import { MetadataContainer } from "../utils/api.ts";
 import { VariantType } from "../utils/variantType.ts";
+import { ConfigCellInfo } from "../types/configCells";
 
 export const VariantInfoTable: Component<{
   variantType: VariantType;
   metadata: MetadataContainer;
   record: Item<VcfRecord>;
+  isShowEmpty: boolean;
 }> = (props) => {
   const configCells = () =>
     initConfigCells(
@@ -25,15 +27,22 @@ export const VariantInfoTable: Component<{
       null,
     ).filter((configCell) => configCell.type !== "group");
 
+  function isEmptyValue(fieldConfig: ConfigCellInfo) {
+    const value = props.record.data.n[fieldConfig.field.id];
+    return value === null || value === undefined || (Array.isArray(value) && value.length === 0);
+  }
+
   return (
     <Table borderless={true}>
       <tbody>
-        <For each={configCells()}>
-          {(fieldConfig) => (
-            <tr>
-              <RecordsTableHeaderCell fieldConfig={fieldConfig} showParentHeader={true} />
-              <RecordsTableCell fieldConfig={fieldConfig} record={props.record} />
-            </tr>
+        <For each={configCells() as ConfigCellInfo[]}>
+          {(fieldConfig: ConfigCellInfo) => (
+            <Show when={!isEmptyValue(fieldConfig) || props.isShowEmpty}>
+              <tr>
+                <RecordsTableHeaderCell fieldConfig={fieldConfig} showParentHeader={true} />
+                <RecordsTableCell fieldConfig={fieldConfig} record={props.record} />
+              </tr>
+            </Show>
           )}
         </For>
       </tbody>
